0f3dea43ec339530223f0e13dacde14c5c91bb9a
syamxm <ahmadsyamim200@gmail.com>
2026-05-09 22:38:18 +0800

    refactor: streamline Firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   if (!kIsWeb) {
     await GoogleSignIn.instance.initialize();
